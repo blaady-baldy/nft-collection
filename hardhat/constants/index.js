2aef43496ee3fb38c01d3cf38b6b9258ffadf9e2
Devansh Singh <devanshsingh201001@gmail.com>
2022-11-05 23:32:00 +0530

    adding front-end

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x7C6f23Cac129EB925a739356C9b7907c245a6367";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
